75f68a4642c410cb1355629f797f7d1eead1c1ec
Marc Tamlyn <marc.tamlyn@gmail.com>
2024-04-16 15:35:19 +0100

    Page titles, fix mobile menu

--- scayt/views.py
+++ scayt/views.py
@@ -6,12 +6,11 @@ from .models import Season
 
 class Root(TemplateView):
     template_name = 'scayt/root.html'
-    page_name = 'root'
 
     def get_context_data(self, **kwargs):
         season = Season.objects.first()
         upcoming_events = season.event_set.order_by('date', 'name').filter(date__gte=timezone.now().date())[:3]
-        return super().get_context_data(season=season, page_name=self.page_name, upcoming_events=upcoming_events, **kwargs)
+        return super().get_context_data(season=season, page_name='root', upcoming_events=upcoming_events, **kwargs)
 
 
 class Calendar(ListView):
@@ -26,9 +25,16 @@ class Calendar(ListView):
         context = super().get_context_data(**kwargs)
         context['season'] = self.season
         context['page_name'] = 'calendar'
+        context['page_title'] = '%s Calendar' % self.season.year
         return context
 
 
 class FAQs(Root):
     template_name = 'scayt/faqs.html'
-    page_name = 'faqs'
+
+    def get_context_data(self, **kwargs):
+        season = Season.objects.first()
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'FAQs'
+        context['page_name'] = 'faqs'
+        return context
