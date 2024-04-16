c1c1eba40ee61354e91ce8d593d46e43413cb84d
Marc Tamlyn <marc.tamlyn@gmail.com>
2024-04-16 13:41:33 +0100

    Might not need whitenoise in the wsgi?

--- scayt/wsgi.py
+++ scayt/wsgi.py
@@ -9,14 +9,8 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 
-from django.conf import settings
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scayt.settings")
 
-application = WhiteNoise(
-    get_wsgi_application(),
-    root=settings.STATIC_ROOT,
-)
-
+application = get_wsgi_application()
