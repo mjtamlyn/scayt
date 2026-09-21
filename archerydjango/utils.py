83fe003207fb6b3a782278fdcd7d9e09def500fa
Marc Tamlyn <marc.tamlyn@gmail.com>
2026-09-21 21:22:33 +0100

    Fix get_age_group for the new enum labels.

--- archerydjango/utils.py
+++ archerydjango/utils.py
@@ -9,17 +9,17 @@ def get_age_group(year_of_birth, year_of_event=None):
     birthday_this_year = year_of_event - year_of_birth
 
     if birthday_this_year < 12:
-        return DbAges.AGE_UNDER_12
+        return DbAges.UNDER_12
     elif birthday_this_year < 14:
-        return DbAges.AGE_UNDER_14
+        return DbAges.UNDER_14
     elif birthday_this_year < 15:
-        return DbAges.AGE_UNDER_15
+        return DbAges.UNDER_15
     elif birthday_this_year < 16:
-        return DbAges.AGE_UNDER_16
+        return DbAges.UNDER_16
     elif birthday_this_year < 18:
-        return DbAges.AGE_UNDER_18
+        return DbAges.UNDER_18
     elif birthday_this_year < 21:
-        return DbAges.AGE_UNDER_21
+        return DbAges.UNDER_21
     elif birthday_this_year >= 50:
-        return DbAges.AGE_50_PLUS
+        return DbAges.OVER_50
     return "Adult"
