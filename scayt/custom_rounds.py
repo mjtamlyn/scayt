8f25021ddd40928992d2dfa20d0050cab9d447c9
Marc Tamlyn <marc.tamlyn@gmail.com>
2026-09-23 21:39:08 +0100

    Add Long Metric VI round.

--- scayt/custom_rounds.py
+++ scayt/custom_rounds.py
@@ -23,4 +23,29 @@ western_20 = Round(
     family="western",
 )
 
-rounds = DotDict({"western_20": western_20})
+long_metric_vi = Round(
+    "Long Metric VI",
+    [
+        Pass.at_target(
+            36,
+            "10_zone",
+            (122, "cm"),
+            (20, "metre"),
+        ),
+        Pass.at_target(
+            36,
+            "10_zone",
+            (122, "cm"),
+            (10, "metre"),
+        ),
+    ],
+    codename="long_metric_vi",
+    location="outdoor",
+    body="AGB",
+    family="long_metric",
+)
+
+rounds = DotDict({
+    "western_20": western_20,
+    "long_metric_vi": long_metric_vi,
+})
